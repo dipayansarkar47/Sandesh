ce7bb5be9d3fc3f6ea074b186ed77f3bee1b1328
dipayansarkar47 <dipayansarkar265482@gmail.com>
2023-04-02 16:15:36 +0530

    updated the UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,35 @@
 import React from 'react'
-import {BsGoogle, BsFacebook} from 'react-icons/bs'
+import { BsGoogle, BsFacebook } from 'react-icons/bs'
 import firebase from 'firebase/compat/app';
 import { auth } from '../firebase'
 const Login = () => {
-  return (
-    <div className='bg-gradient-to-t from-gray-900 to-blue-950'>
-        <div className='flex flex-col gap-5 text-white justify-center items-center h-screen'>
-            <h2 className='text-3xl lg:text-6xl font-bold'>Welcome to Sandesh!</h2>
-            <div className='bg-blue-700 mt-8 w-64 lg:w-96 lg:h-20 text-white rounded-xl flex flex-row justify-center items-center h-16 gap-2 hover:scale-105 duration-300'
-                onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
-            >
-                <BsGoogle className='' /> Sign in with Google
-            </div>
-            <div className='bg-blue-950 text-white w-64 lg:w-96 lg:h-20 rounded-xl flex flex-row justify-center items-center h-16 gap-2 hover:scale-105 duration-300'
-                onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
-            >
-                <BsFacebook /> Sign in with Facebook
+    return (
+        <div className='bg-gray-800'>
+            <div className='flex flex-col gap-5 text-white justify-center items-center h-screen'>
+                <h1 className="mb-2 font-mono text-4xl text-gray-100 md:text-6xl">
+                    Welcome to <br className="block md:hidden" />
+                    <span className="relative">
+                        <span className="h-20 pt-2 font-bold overflow-x-hidden whitespace-nowrap text-brand-accent text-orange-500">
+                            Sandesh <span className="text-3xl md:text-5xl">💬</span>
+                        </span>
+                        <span
+                            className="{`${styles.cursor} absolute -bottom-0 left-0 -top-1 inline-block bg-gray-800 w-full animate-type will-change`}"
+                        ></span>
+                    </span>
+                </h1>
+                <div className='bg-blue-700 mt-8 w-64 lg:text-2xl lg:w-96 lg:h-20 text-white rounded-xl flex flex-row justify-center items-center h-16 gap-2 hover:scale-105 duration-300'
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
+                >
+                    <BsGoogle className='' /> Sign in with Google
+                </div>
+                <div className='bg-blue-900 text-white w-64 lg:text-2xl lg:w-96 lg:h-20 rounded-xl flex flex-row justify-center items-center h-16 gap-2 hover:scale-105 duration-300'
+                    onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
+                >
+                    <BsFacebook /> Sign in with Facebook
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        type: 'type 2.7s ease-out .8s infinite alternate both',
+      },
+      keyframes: {
+        type: {
+          '0%': { transform: 'translateX(0ch)' },
+          '5%, 10%': { transform: 'translateX(1ch)' },
+          '15%, 20%': { transform: 'translateX(2ch)' },
+          '25%, 30%': { transform: 'translateX(3ch)' },
+          '35%, 40%': { transform: 'translateX(4ch)' },
+          '45%, 50%': { transform: 'translateX(5ch)' },
+          '55%, 60%': { transform: 'translateX(6ch)' },
+          '65%, 70%': { transform: 'translateX(7ch)' },
+          '75%, 80%': { transform: 'translateX(8ch)' },
+          '85%, 90%': { transform: 'translateX(9ch)' },
+          '95%, 100%': { transform: 'translateX(11ch)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
